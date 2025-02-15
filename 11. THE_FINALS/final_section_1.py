d63479f05db6e7a2a2d94358df3b0ff485ceee18
Fonyuy Gita <FonyuyJudegita@gmail.com>
2025-02-15 03:32:07 -0800

    added finals exercise

---
+++ 11. THE_FINALS/final_section_1.py
@@ -0,0 +1,10 @@
+# Question 1 : Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters. Suppose the following input is supplied to the program: Hello world! Then, the output should be: UPPER CASE 1 LOWER CASE 9
+
+# Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
+
+# Question 2
+
+
+# Question A robot moves in a plane starting from the original point (0,0). The robot can move toward UP, DOWN, LEFT and RIGHT with a given steps. The trace of robot movement is shown as the following: UP 5 DOWN 3 LEFT 3 RIGHT 2 ¡­ The numbers after the direction are steps. Please write a program to compute the distance from current position after a sequence of movement and original point. If the distance is a float, then just print the nearest integer. Example: If the following tuples are given as input to the program: UP 5 DOWN 3 LEFT 3 RIGHT 2 Then, the output of the program should be: 2
+
+# Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
